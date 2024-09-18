9aa430a7d0a920d1332b28937325098e78123491
PTJohn0122 <xingjian_hao@brown.edu>
2024-09-18 01:53:10 -0400

    nullptr

---
+++ pointers/nullptr.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 郝行健 on 9/17/24.
+// functions are assigned to obj while compiling, even if it's nullptr. Since this function dosen't use 'this', it
+// won't have segmentation fault
+//
+#include <iostream>
+using namespace std;
+
+class Car {
+private:
+public:
+    void drive() {
+        cout << "The car is vrooming" << endl;
+    }
+    ~Car() = default;
+};
+
+int main() {
+    Car *myCar = nullptr;
+    myCar->drive();
+    delete myCar;
+
+    return 0;
+}
